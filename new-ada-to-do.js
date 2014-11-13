9e7aeba25e645cc77a0b0f268ab7f68257b596c7
Rachel <rmoshier@gmail.com>
2014-11-13 14:18:07 -0800

    too many changes

--- new-ada-to-do.js
+++ new-ada-to-do.js
@@ -1,24 +1,32 @@
-$(function () {
+  $(function () {
 
-  for ( i = 1; i < 4; i++ ){
-    $("#list").append("<li>Sample item</li>");
-  }
-
-  $("#addTask").click(function() {
-    event.preventDefault();
-    var task = $("#task").val();
-
-    if (task !== "null" && task !== "") {
-      var $listItem = $("<li>" + task + "</li>");
-
-      $listItem.click(function(){
-        $(this).toggleClass("done");
-      });
-
-      $("#list").append($listItem);
-      $($newListItem).hide().fadeIn("fast");
+    for ( i = 1; i < 4; i++ ){
+      $("#list").append("<li>Sample item</li>");
     }
 
-    document.getElementById("task").value = "";
+    $("#addTask").click(function() {
+      event.preventDefault();
+
+      var task = $("#task").val();
+      $("#task").val(null);
+
+      if (task !== "null" && task !== "") {
+        var $listItem = $("<li>" + task + "</li>");
+
+        $listItem.click(function(){
+          $(this).toggleClass("done");
+          removeCheck();
+        });
+
+        $("#list").append($listItem);
+        $($listItem).hide().fadeIn("slow");
+        $("ul").sortable();
+      }
+
+    });
+
+    $( "#list" ).click(function() {
+      $( "#li" ).remove();
+    });
+
   });
-});
