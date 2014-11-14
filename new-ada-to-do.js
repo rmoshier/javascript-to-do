5d0210f67901178e2d4cef7204249ce2d84ef962
Rachel <rmoshier@gmail.com>
2014-11-13 16:38:17 -0800

    more

--- new-ada-to-do.js
+++ new-ada-to-do.js
@@ -26,7 +26,13 @@
     });
 
   $(function() {
-      $( "#list1" ).draggable();
+      $( "#list1" ).draggable(
+        // cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+        // revert: "invalid", // when not dropped, the item will revert back to its initial position
+        // containment: "document",
+        // helper: "clone",
+        // cursor: "move"
+      );
       $( "#list2" ).droppable({
         drop: function( event, ui ) {
           $( this )
